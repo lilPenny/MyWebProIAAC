b26b6a190f2f6b2449ca59db0d1acb9e06054804
lilPenny <asdmx@icloud.com>
2025-03-12 14:47:29 +0100

    init commit

---
+++ SecGrp.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "web-sg" {
+  name        = "web-sg"
+  description = "web-sg inbound traffic and outbound traffic"
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+data "aws_security_group" "exising-sg" {
+  name = "ansible-sg"
+  
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sshFromMyIp" {
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = var.myIp
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sshfromexisting" {
+  security_group_id            = aws_security_group.web-sg.id
+  referenced_security_group_id = data.aws_security_group.exising-sg.id
+  from_port                    = 22
+  ip_protocol                  = "tcp"
+  to_port                      = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv6" {
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "myIp" {
+  description = "value of my ip"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "zone" {
+  default = "us-east-1a"
+}
+
+# In case if I want to specify specific AMI for each region
+variable "amiId" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-04b4f1a9cf54c11d0"
+    "us-east-2" = ""
+  }
+}
