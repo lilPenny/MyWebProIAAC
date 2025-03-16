061caab33a4de309ed5e7c38f2a3be7e2b09ba71
lilPenny <asdmx@icloud.com>
2025-03-16 20:23:07 +0100

    Iam role for SSM added

---
+++ iam.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "ec2SsmRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2SsmProfile"
+  role = aws_iam_role.ec2_role.name
+}
--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami                    = var.amiId[var.region] != "" ? var.amiId[var.region] : data.aws_ami.amiId[0].id
   instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name               = "instanceKey"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   availability_zone      = var.zone
