e75ed410d5c5c32f126f1f003c85d2543128ae5a
lilPenny <asdmx@icloud.com>
2025-03-19 21:58:20 +0100

    sec grp allow from my ip for 80 and 443 for testing

--- terraform/secGrp.tf
+++ terraform/secGrp.tf
@@ -29,10 +29,18 @@ resource "aws_vpc_security_group_ingress_rule" "sshfromexisting" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.web-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.myIp
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = var.myIp
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
