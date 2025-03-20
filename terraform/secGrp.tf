6ab514ed938e32bf0e1703bab4a4c83c76a41a7c
lilPenny <asdmx@icloud.com>
2025-03-20 22:05:50 +0100

    cloduflare ips added test

--- terraform/secGrp.tf
+++ terraform/secGrp.tf
@@ -7,24 +7,12 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-data "aws_security_group" "exising-sg" {
-  name = "ansible-sg"
-}
-
 resource "aws_vpc_security_group_ingress_rule" "sshFromMyIp" {
   security_group_id = aws_security_group.web-sg.id
   cidr_ipv4         = var.myIp
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
-}
-
-resource "aws_vpc_security_group_ingress_rule" "sshfromexisting" {
-  security_group_id            = aws_security_group.web-sg.id
-  referenced_security_group_id = data.aws_security_group.exising-sg.id
-  from_port                    = 22
-  ip_protocol                  = "tcp"
-  to_port                      = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
@@ -35,9 +23,27 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  for_each          = toset(var.cloudflare_ips)
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = each.value
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.web-sg.id
   cidr_ipv4         = var.myIp
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  for_each          = toset(var.cloudflare_ips)
+  security_group_id = aws_security_group.web-sg.id
+  cidr_ipv4         = each.value
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
