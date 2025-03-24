0130e6541586d24d0f9a8befa17b72aa7866520c
lilPenny <asdmx@icloud.com>
2025-03-24 16:37:34 +0100

    added cron job

--- cron/dnsupdate.sh
+++ cron/dnsupdate.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
-set -a
 source /WebServer/traefik/.env
-set +a
 
-echo $EMAIL
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+PUBLIC_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 
 curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID \
     -X PUT \
     -H 'Content-Type: application/json' \
     -H "X-Auth-Email: $EMAIL" \
-    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+    -H "Authorization: Bearer $CF_DNS_API_TOKEN" \
     -d '{
       "comment": "Domain verification record",
-      "content": "198.51.100.4",
-      "name": "example.com",
+      "content": "'$PUBLIC_IP'",
+      "name": "pennyxz.com",
       "proxied": true,
       "ttl": 3600,
       "type": "A"
-    }' -y
+    }'
